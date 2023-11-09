881a62ec9087f8021ab9fde9d3ccd5dd87b019e9
PedroPaulo00 <92110338+PedroPaulo00@users.noreply.github.com>
2023-11-09 10:39:34 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAiNkuPcCARsgIO7xiH26dbbf6jUsryY3c",
+    authDomain: "agendacontatos-738f8.firebaseapp.com",
+    projectId: "agendacontatos-738f8",
+    storageBucket: "agendacontatos-738f8.appspot.com",
+    messagingSenderId: "74875878679",
+    appId: "1:74875878679:web:8af1a591f94c700ee8cb52"
+  }
+};
